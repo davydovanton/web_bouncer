b01a59607ca4336738fe662ae61d4cc1eef1c351
Anton Davydov <antondavydov.o@gmail.com>
2017-04-09 15:25:42 +0300

    Add rack-test for testing roda tree

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,28 @@ require 'bundler/setup'
 Bundler.setup
 
 require 'web_bouncer'
+require 'rack/test'
+
+ENV['RACK_ENV'] = 'test'
+
+class TestingApp < Roda
+  use Rack::Session::Cookie, secret: '123'
+
+  use WebBouncer::Middleware, {
+    model: :account,
+    login_redirect: '/',
+    logout_redirect: '/'
+  }
+end
+
+module RSpecMixin
+  include Rack::Test::Methods
+  def app() TestingApp end
+end
 
 RSpec.configure do |config|
+  config.include RSpecMixin
+
   config.filter_run_when_matching :focus
   config.disable_monkey_patching!
   config.warnings = false
