724acd74ce56a49b58d17307ff160edf62fdc591
Anton Davydov <antondavydov.o@gmail.com>
2017-04-09 15:04:33 +0300

    Add simple tests for OauthContainer class

--- spec/oauth_container_spec.rb
+++ spec/oauth_container_spec.rb
@@ -1,5 +1,37 @@
 require 'spec_helper'
+require 'dry-monads'
 
 RSpec.describe WebBouncer::OauthContainer do
-  pending "write it"
+  include Dry::Monads::Either::Mixin
+
+  let(:container) { WebBouncer::OauthContainer }
+
+  describe 'when user rewrite container' do
+    class TestContainer < WebBouncer::OauthContainer
+      register 'oauth.base_callback' do
+        Right('changed')
+      end
+    end
+
+    class WebBouncer::OauthContainer
+      register 'oauth.facebook_callback' do
+        Right('facebook account')
+      end
+    end
+
+    it { expect(TestContainer['oauth.base_callback'].call).to eq Right('changed') }
+    it { expect(container['oauth.facebook_callback'].call).to eq Right('facebook account') }
+  end
+
+  describe 'oauth.failure container' do
+    it { expect(container['oauth.failure'].call).to eq Right(nil) }
+  end
+
+  describe 'oauth.logout container' do
+    it { expect(container['oauth.logout'].call).to eq Right(nil) }
+  end
+
+  describe 'oauth.base_callback container' do
+    it { expect(container['oauth.base_callback'].call).to eq Right('account object') }
+  end
 end
