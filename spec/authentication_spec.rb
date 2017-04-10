b22c01f65d81643c149409f4345aa3715ec8ce55
Anton Davydov <antondavydov.o@gmail.com>
2017-04-10 03:30:15 +0300

    Add more tests for controller helpers

--- spec/authentication_spec.rb
+++ spec/authentication_spec.rb
@@ -4,9 +4,30 @@ require 'support/action_classes'
 
 RSpec.describe WebBouncer::Authentication do
   let(:action) { Action.new(account: account) }
-  let(:account){ Account.new(nil) }
+  let(:account){ Account.new(id: 1) }
 
   describe '#current_account' do
-    it { expect(action.current_account).to eq account }
+    context 'when account does not exist' do
+      let(:action) { Action.new(account: nil) }
+      let(:account){ Account.new(nil) }
+
+      it { expect(action.current_account).to eq account }
+    end
+
+    context 'when account exist' do
+      it { expect(action.current_account).to eq account }
+    end
+  end
+
+  describe '#authenticated?' do
+    context 'when account does not exist' do
+      let(:action) { Action.new(account: nil) }
+
+      it { expect(action.authenticated?).to eq false }
+    end
+
+    context 'when account exist' do
+      it { expect(action.authenticated?).to eq true }
+    end
   end
 end
