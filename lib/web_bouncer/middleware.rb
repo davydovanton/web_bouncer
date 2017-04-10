eb4c9c1bfcaa2f07dfe95668f7535a0f06353e06
Anton Davydov <antondavydov.o@gmail.com>
2017-04-10 04:05:16 +0300

    Add allow_oauth option

--- lib/web_bouncer/middleware.rb
+++ lib/web_bouncer/middleware.rb
@@ -2,10 +2,15 @@ require 'roda'
 
 module WebBouncer
   class Middleware < Roda
+    DEFAULT_CONFIG = {
+      model: :account,
+      login_redirect: '/',
+      logout_redirect: '/',
+      allow_oauth: true
+    }
+
     plugin :middleware do |middleware, config = {}, &block|
-      config[:model] ||= :account
-      config[:login_redirect] ||= '/'
-      config[:logout_redirect] ||= '/'
+      config = DEFAULT_CONFIG.merge(config)
 
       middleware.opts[:config] = config
       block.call(middleware) if block
@@ -14,46 +19,49 @@ module WebBouncer
     route do |r|
       config = opts[:config]
 
-      r.is 'auth/failure' do
-        Matcher.call(OauthContainer['oauth.failure'].call) do |m|
-          m.success do |v|
-            "Successed #{v}"
-          end
+      if config[:allow_oauth]
+        r.is 'auth/failure' do
+          Matcher.call(OauthContainer['oauth.failure'].call) do |m|
+            m.success do |v|
+              "Successed #{v}"
+            end
 
-          m.failure do |v|
-            "Failed #{v}"
-          end
-        end
-      end
-
-      r.is 'auth/logout' do
-        Matcher.call(OauthContainer['oauth.logout'].call) do |m|
-          m.success do |value|
-            session[config[:model]] = value
-          end
-
-          m.failure do |v|
+            m.failure do |v|
+              "Failed #{v}"
+            end
           end
         end
 
-        r.redirect config[:logout_redirect]
-      end
+        r.is 'auth/logout' do
+          Matcher.call(OauthContainer['oauth.logout'].call) do |m|
+            m.success do |value|
+              session[config[:model]] = value
+            end
 
-      r.on 'auth/:provider/callback' do |provider|
-        action = OauthContainer["oauth.#{provider}_callback"] || OauthContainer["oauth.base_callback"]
-        data = request.env['omniauth.auth']
-
-        Matcher.call(action.call(data)) do |m|
-          m.success do |value|
-            session[config[:model]] = value
+            m.failure do |v|
+            end
           end
 
-          m.failure do |v|
-          end
+          r.redirect config[:logout_redirect]
         end
 
-        r.redirect config[:login_redirect]
+        r.on 'auth/:provider/callback' do |provider|
+          action = OauthContainer["oauth.#{provider}_callback"] || OauthContainer["oauth.base_callback"]
+          data = request.env['omniauth.auth']
+
+          Matcher.call(action.call(data)) do |m|
+            m.success do |value|
+              session[config[:model]] = value
+            end
+
+            m.failure do |v|
+            end
+          end
+
+          r.redirect config[:login_redirect]
+        end
       end
+
     end
   end
 end
