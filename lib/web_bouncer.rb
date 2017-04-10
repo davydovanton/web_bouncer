3c650afd349b7e5e8782aa5a604b1e3f9305128a
Anton Davydov <antondavydov.o@gmail.com>
2017-04-10 04:31:47 +0300

    Wrap all app to container

--- lib/web_bouncer.rb
+++ lib/web_bouncer.rb
@@ -1,8 +1,18 @@
 require "web_bouncer/oauth_container"
+require "web_bouncer/authentication"
 require "web_bouncer/middleware"
 require "web_bouncer/version"
+
 require "dry/matcher/either_matcher"
+require 'dry-container'
 
 module WebBouncer
   Matcher = Dry::Matcher::EitherMatcher
+
+  class Container
+    extend ::Dry::Container::Mixin
+
+    register 'middleware', WebBouncer::Middleware
+    register 'authentication', WebBouncer::Authentication
+  end
 end
